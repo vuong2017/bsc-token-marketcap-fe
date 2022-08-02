d60e46b73e64c281ee15298b549229cbcf36a271
Vuong Phan <vuong.pq@terra-drone.co.jp>
2022-08-02 16:06:02 +0700

    remove redundant code

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,41 +1,14 @@
-// profile
-import profilePage from '../pages/profile/ProfilePage.vue'
-import profilePostsPage from '../pages/profile/ProfilePostsPage.vue'
-
 // routes
 import auth from '@/pages/login/router'
 import wallet from '@/pages/wallet/router'
-// single pages
-import newsPage from '@/pages/news/NewsPage.vue'
-import notFoundPage from '@/pages/NotFound.vue'
-
-import { routePropResolver } from './util'
+import NotFoundPage from '@/pages/NotFound.vue'
 
 export const routes = [
-  {
-    path: '/news',
-    name: 'news',
-    component: newsPage,
-    meta: { title: 'News' },
-    props: routePropResolver
-  },
-  {
-    path: '/profile',
-    component: profilePage,
-    meta: { isAuth: true, title: 'Profile' },
-    children: [
-      {
-        path: '',
-        name: 'profile',
-        component: profilePostsPage
-      }
-    ]
-  },
   ...auth,
   ...wallet,
   {
     path: '*',
-    component: notFoundPage,
+    component: NotFoundPage,
     meta: { title: 'Not found' }
   }
 ]
